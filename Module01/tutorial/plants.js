a59718f74acf6999785ed0b48b540871dfd2e672
Capt-Obviouse <sixstringjesse@gmail.com>
2018-09-24 22:48:42 -0400

    initial commit

---
+++ Module01/tutorial/plants.js
@@ -0,0 +1,16 @@
+/* JavaScript 6th Edition
+    Chapter 1
+    Chapter case
+
+
+    Tinley Xeriscapes
+    Author: Jesse Duncan
+    Date: 9/4/2018
+
+    Filename: plants.js
+*/
+
+//define variables containing img src values
+var blanket = "images/blanket.jpg";
+var bluestem = "images/bluestem.jpg";
+var rugosa = "images/rugosa.jpg";
